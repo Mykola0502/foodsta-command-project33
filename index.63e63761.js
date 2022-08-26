5f661a0482d7e1b25e4fc556660eb68373d4a6a1
Mykola0502 <107276054+Mykola0502@users.noreply.github.com>
2022-08-26 08:39:26 +0000

    Deploying to gh-pages from @ Mykola0502/foodsta-command-project33@b19d97b52b663070ea684e5068f10d9ba69faf7f 🚀

---
+++ index.63e63761.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),c=document.querySelector("[data-menu]"),n=document.querySelector("body");function o(){c.classList.toggle("is-open"),n.classList.toggle("no-scroll")}e.addEventListener("click",o),t.addEventListener("click",o)}();
+//# sourceMappingURL=index.63e63761.js.map
