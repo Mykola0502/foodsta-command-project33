7f33e73fdef3fbe77c45ac61d629213b099514b9
Mykola Saliuk <haker0502@ukr.net>
2022-08-29 02:48:16 +0300

    фіксить слайдер

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,21 @@
-var swiper = new Swiper(".mySwiper", {
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                },
-            });
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
